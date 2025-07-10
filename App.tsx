10b686095357edf9db21e2460fb768ac3d4cc5f9
Rami Elwan <ramielwan48@gmail.com>
2025-07-10 14:39:37 +0200

    @react-native-menu/menu

--- App.tsx
+++ App.tsx
@@ -1,17 +1,86 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { MenuView } from '@react-native-menu/menu';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
-        <Text>Lorem ipsum dolor sit amet</Text>
-        <Text>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo.
-        </Text>
-      </View>
+      <MenuView
+        title="Menu Title"
+        onPressAction={({ nativeEvent }) => {
+          console.warn(JSON.stringify(nativeEvent));
+        }}
+        actions={[
+          {
+            id: 'add',
+            title: 'Add',
+            titleColor: '#2367A2',
+            image: Platform.select({
+              ios: 'plus',
+              android: 'ic_menu_add',
+            }),
+            imageColor: '#2367A2',
+            subactions: [
+              {
+                id: 'nested1',
+                title: 'Nested action',
+                titleColor: 'rgba(250,180,100,0.5)',
+                subtitle: 'State is mixed',
+                image: Platform.select({
+                  ios: 'heart.fill',
+                  android: 'ic_menu_today',
+                }),
+                imageColor: 'rgba(100,200,250,0.3)',
+                state: 'mixed',
+              },
+              {
+                id: 'nestedDestructive',
+                title: 'Destructive Action',
+                attributes: {
+                  destructive: true,
+                },
+                image: Platform.select({
+                  ios: 'trash',
+                  android: 'ic_menu_delete',
+                }),
+              },
+            ],
+          },
+          {
+            id: 'share',
+            title: 'Share Action',
+            titleColor: '#46F289',
+            subtitle: 'Share action on SNS',
+            image: Platform.select({
+              ios: 'square.and.arrow.up',
+              android: 'ic_menu_share',
+            }),
+            imageColor: '#46F289',
+            state: 'on',
+          },
+          {
+            id: 'destructive',
+            title: 'Destructive Action',
+            attributes: {
+              destructive: true,
+            },
+            image: Platform.select({
+              ios: 'trash',
+              android: 'ic_menu_delete',
+            }),
+          },
+        ]}
+        shouldOpenOnLongPress={true}
+      >
+        <View style={styles.card}>
+          <Text>Lorem ipsum dolor sit amet</Text>
+          <Text>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo.
+          </Text>
+        </View>
+      </MenuView>
     </View>
   );
 }
