8bea6955e285bc506e88a605b5fcfbd210f7e712
Rami Elwan <ramielwan48@gmail.com>
2025-07-10 13:48:06 +0200

    use zeego

--- App.tsx
+++ App.tsx
@@ -1,17 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native';
+import * as ContextMenu from 'zeego/context-menu';
 
 function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
-        <Text>Lorem ipsum dolor sit amet</Text>
-        <Text>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo.
-        </Text>
-      </View>
+      <ContextMenu.Root>
+        <ContextMenu.Trigger>
+          <View style={styles.card}>
+            <Text>Lorem ipsum dolor sit amet</Text>
+            <Text>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo.
+            </Text>
+          </View>
+        </ContextMenu.Trigger>
+        <ContextMenu.Content>
+          <ContextMenu.Label />
+          <ContextMenu.Item key="1">
+            <ContextMenu.ItemTitle>Item 1</ContextMenu.ItemTitle>
+          </ContextMenu.Item>
+        </ContextMenu.Content>
+      </ContextMenu.Root>
     </View>
   );
 }
